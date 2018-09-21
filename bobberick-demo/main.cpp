9affe86758da779ecbe84ebb9f0943e93cdde48f
Kevin van Rossum <k.van.rossum@iblux.nl>
2018-09-21 20:05:08 +0200

    refactored main method a little

--- bobberick-demo/main.cpp
+++ bobberick-demo/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include "../bobberick-framework/src/Game.h"
 
-Game* game = nullptr;
-const int FPS = 60;
-float DELAY_TIME = 1000.0f / FPS;
+Game *game = nullptr;
 
-int main(int argc, char **argv)
-{
-    Uint32 frameStart, frameTime;
+int main(int argc, char **argv) {
+    const int FPS = 60;
+    const int DELAY_TIME = 1000 / FPS;
+
+    Uint32 frameStart;
+    int frameTime;
 
     game = new Game();
 
-    if (game->init("Chaptere 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, static_cast<Uint32>(false))) {
+    if (game->init("Chaptere 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480,
+                   static_cast<Uint32>(false))) {
 
         while (game->running()) {
             frameStart = SDL_GetTicks();
@@ -21,7 +23,7 @@ int main(int argc, char **argv)
             frameTime = SDL_GetTicks() - frameStart;
 
             if (frameTime < DELAY_TIME) {
-                SDL_Delay(static_cast<Uint32>((int) (DELAY_TIME - frameTime)));
+                SDL_Delay(static_cast<Uint32>(DELAY_TIME - frameTime));
             }
         }
     } else {
