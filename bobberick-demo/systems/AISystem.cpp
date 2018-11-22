5734f1cd3684f5b15c6dc96c8db04d205be813d8
WouterBouwman <wouter.bouwman@hotmail.com>
2018-11-22 11:50:50 +0100

    Moved addSpaces to a seperate util class

--- bobberick-demo/systems/AISystem.cpp
+++ bobberick-demo/systems/AISystem.cpp
@@ -513,17 +513,3 @@ void AISystem::applyMovement(Entity& entity)
 		collisionComponent.collider.h = transform.height;
 	}
 }
-
-std::string AISystem::addSpaces(const std::string& string, const int goalChars, const bool leading)
-{
-	std::string spaces;
-	for (int i = string.length(); i < goalChars; i++)
-	{
-		spaces += " ";
-	}
-	if (leading)
-	{
-		return spaces + string;
-	}
-	return string + spaces;
-}
