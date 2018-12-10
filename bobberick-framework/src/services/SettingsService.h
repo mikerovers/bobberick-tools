d3214a3fa177d05b9cbf20137460bc305ce1f95e
thimh <thim.heider@gmail.com>
2018-12-10 23:25:45 +0100

    Added key-bindings to SettingsService, key mapping not done yet.

--- bobberick-framework/src/services/SettingsService.h
+++ bobberick-framework/src/services/SettingsService.h
@@ -18,6 +18,32 @@ public:
 	int advertisementCount;
 	bool music = true;
 	bool fps = false;
+
+	// Movement key-bindings
+	SDL_Scancode left1 = SDL_SCANCODE_LEFT;
+	SDL_Scancode right1 = SDL_SCANCODE_RIGHT;
+	SDL_Scancode up1 = SDL_SCANCODE_UP;
+	SDL_Scancode down1 = SDL_SCANCODE_DOWN;
+	SDL_Scancode left2 = SDL_SCANCODE_A;
+	SDL_Scancode right2 = SDL_SCANCODE_D;
+	SDL_Scancode up2 = SDL_SCANCODE_W;
+	SDL_Scancode down2 = SDL_SCANCODE_S;
+
+	// FPS key-bindings
+	SDL_Scancode fpsSpdUp = SDL_SCANCODE_Z;
+	SDL_Scancode fpsSpdDown = SDL_SCANCODE_X;
+	SDL_Scancode fpsSpdReset = SDL_SCANCODE_C;
+
+	// Equip comparing weapon key-bindings
+	SDL_Scancode equipWeapon1 = SDL_SCANCODE_LSHIFT;
+	SDL_Scancode equipWeapon2 = SDL_SCANCODE_RSHIFT;
+
+	// Stop game key-bindings
+	SDL_Scancode stopGame1 = SDL_SCANCODE_RETURN;
+	SDL_Scancode stopGame2 = SDL_SCANCODE_RETURN2;
+
+	// Pause key-bindings
+	SDL_Scancode pauseGame = SDL_SCANCODE_ESCAPE;
 };
 
 
