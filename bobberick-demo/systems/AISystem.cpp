cfaa98f51ccc6f30f973d04d96378e0dc9437c65
Roel Wolters <r.wolters@student.avans.nl>
2018-12-18 14:43:48 +0100

    Boss health bar issue fix

--- bobberick-demo/systems/AISystem.cpp
+++ bobberick-demo/systems/AISystem.cpp
@@ -498,11 +498,11 @@ void AISystem::kill(Entity& entity)
 		auto& enemyTransform = enemy.getComponent<TransformComponent>();
 		enemyTransform.position.x = killedTransform.position.x;
 		enemyTransform.position.y = killedTransform.position.y + 50;
-		initHealthBar(enemy);
 		for (const auto& group : entity.getGroups())
 		{
 			ServiceManager::Instance()->getService<EntityManager>().addEntityToGroup(enemy, group);
 		}
+		initHealthBar(enemy);
 		auto endBossEntities = ServiceManager::Instance()->getService<EntityManager>().getAllEntitiesWithComponent<EndBossComponent>();
 		if (endBossEntities.size() <= 1) {
 			ServiceManager::Instance()->getService<SoundManager>().playMusic("boss", -1);
